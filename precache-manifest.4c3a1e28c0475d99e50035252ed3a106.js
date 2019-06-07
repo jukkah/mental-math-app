4504ed2cc5c1c74c9eeab634d42ba75f4d40d555
Jukka Hyytiälä <jukka.hyytiala@suoratyo.fi>
2019-06-07 11:11:07 +0300

    Updates

---
+++ precache-manifest.4c3a1e28c0475d99e50035252ed3a106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53634249a947280dd50efa513f8860c",
+    "url": "/mental-math-app/index.html"
+  },
+  {
+    "revision": "a06ffa4e43a04579d0e4",
+    "url": "/mental-math-app/static/css/main.ac96e083.chunk.css"
+  },
+  {
+    "revision": "096a74acc344d2da2d3f",
+    "url": "/mental-math-app/static/js/2.aa77813f.chunk.js"
+  },
+  {
+    "revision": "a06ffa4e43a04579d0e4",
+    "url": "/mental-math-app/static/js/main.cf2c8448.chunk.js"
+  },
+  {
+    "revision": "751f71b8099d1ca99c16",
+    "url": "/mental-math-app/static/js/runtime~main.318e07eb.js"
+  }
+]);
